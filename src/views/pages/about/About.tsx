b76395a053194f38439c93641de4cffd1a6bf6c9
AyuburRahman <aiburrahman17@gmail.com>
2022-04-01 12:06:38 +0600

    added new

--- src/views/pages/about/About.tsx
+++ src/views/pages/about/About.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const About = () => {
     return (
         <div>
-            <h2>Thsi is About page</h2>
+            <h2>Thsi is About page again</h2>
         </div>
     );
 };
