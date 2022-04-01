3b6a94f694e343c233f79753dab15aea2010f4d7
rakibshakib <rakibshakib74@gmail.com>
2022-04-02 00:40:26 +0600

    update navbar and header booking form only design section complete

--- src/views/pages/common/header/Header.tsx
+++ src/views/pages/common/header/Header.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
+import BookingForm from '../../../components/common/BookingForm';
 import Navbar from '../Navbar/Navbar';
 
 const Header = () => {
     return (
-        <div>
+        <div className='bg-black'>
             <Navbar />
             {/* booking form  */}
-            <div className='container mx-auto'>
-                this is booking form 
+            <div className='container mx-auto my-5 bg-black text-white'>
+                <h2 className='text-2xl font-bold text-center my-5'>PLease Set your Destination</h2>
+                <div className="form_container">
+                    <BookingForm />
+                </div>
             </div>
         </div>
     );
