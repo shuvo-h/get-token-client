5b16d342fbddef146137b46bb5e8e18ae00fa224
Shuvo Haldar <86658217+shuvo-h@users.noreply.github.com>
2022-04-05 09:41:39 +0600

    axios instance created

---
+++ src/services/http.services.ts
@@ -0,0 +1,36 @@
+import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from "axios";
+
+const axiosConfig:AxiosRequestConfig = {
+    baseURL: process.env.REACT_APP_API_BASE_URL,
+    timeout: 15000
+}
+
+const instance:AxiosInstance = axios.create(axiosConfig);
+
+instance.interceptors.request.use(function(config){
+    return {
+        ...config,
+        headers:{Authorization: `Bearer ${"access_token"}`}
+    }
+}, function(error){
+    return Promise.reject(error)
+})
+
+class Request {
+    async get(url:string):Promise<AxiosResponse>{
+        return instance.get(url).then(res=>res)
+    }
+    async post(url:string):Promise<AxiosResponse>{
+        return instance.post(url).then(res=>res)
+    }
+    async update(url:string):Promise<AxiosResponse>{
+        return instance.patch(url).then(res=>res)
+    }
+    async delete(url:string):Promise<AxiosResponse>{
+        return instance.delete(url).then(res=>res)
+    }
+}
+
+const httpReq = new Request();
+
+export default httpReq;
