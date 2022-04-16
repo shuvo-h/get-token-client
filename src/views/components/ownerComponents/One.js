d03eebe184f61e5c1f3c43a446bde035d75ac47a
AyuburRahman <aiburrahman17@gmail.com>
2022-04-16 14:24:04 +0600

    added hotel owner dashboard

--- src/views/components/ownerComponents/One.js
+++ src/views/components/ownerComponents/One.js
@@ -1,13 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import Pichart2 from './Pichart2';
+// import Piechart from './Piechart';
+import arrow from '../../../assets/images/arrow.jpg';
 import Piechart from './Piechart';
-import Reservation from './Reservation';
+
 
 
 
 const One = () => {
+    const [open, setOpen] = useState(true);
+    const Menus = [
+        {title:"Dashboard", src:""},
+        {title:"Email", src:""},
+        {title:"Invoices", src:""},
+        {title:"Total", src:""},
+        {title:"Media", src:""},
+        {title:"Setting", src:""},
+    ]
     return (
-        <div className="">
+     <div className='flex'>
+         <div className={`${open ? "w-72" : "w-20"} duration-300 h-screen p-5 pt-8 bg-gray-800 relative`} >
+         <img src={arrow} alt="" className={`absolute cursor-pointer rounded-full -right-3 top-9 w-7 border-2 border-dark-purple  ${!open && "rotate-180"}`} 
+           onClick={() => setOpen(!open)}
+         />
+         <div className='flex gap-x-4 items center' >
+             < img src='' className='cursor-pointer duration-500' />
+             <h1 className={`text-white origin-left font-medium text-xl duration-300 ${!open && 'scale-0'}`} >Dashboard</h1>
+         </div>
+              <ul className='pt-6' >
+                 {Menus.map((menu, index) => (
+                    <li key={index} className={`text-gray-300 text-sm flex items-center gap-x-4 cursor-pointer p-2 hover:bg-light-white rounded-md ${menu.gap ? "mt-9" : "mt-2" } `} >
+                      < img src='' /> 
+                      <span>{menu.title}</span>  
+                        
+                        </li> 
+                 ))} 
+              </ul> 
+
+         </div>
+         <div className='p-7 text-2xl font-semibold flex-1 h-screen' >
+             <h1>Home Page</h1>
+
+             <div className="">
             
             <div className='flex-auto '>
                 <div className=' flex flex-col bg-white'>
@@ -57,20 +91,14 @@ const One = () => {
                         <div className=''><Piechart></Piechart></div>
                         <div className=''><Piechart></Piechart></div>
                         <div className=''><Piechart></Piechart></div>
-                        {/* <div className=''><Pichart2></Pichart2></div> */}
+                        
                      </div>
                  </div>
             </div>
 
-            <div>
-                <div>
-                    <Reservation></Reservation>
-                </div>
-            </div>
-
          </div>
-
-        
+         </div>
+     </div>
     );
 };
 
