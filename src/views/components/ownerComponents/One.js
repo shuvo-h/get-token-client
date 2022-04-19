61e5309377c3163d8af230fb40aba37f3d7c13a6
AyuburRahman <aiburrahman17@gmail.com>
2022-04-19 19:39:22 +0600

    added something

--- src/views/components/ownerComponents/One.js
+++ src/views/components/ownerComponents/One.js
@@ -64,13 +64,13 @@ const One = () => {
         ))}
       </ul>
     </div>
-    <div className="h-screen flex-1 p-7">
+    {/* <div className="h-screen flex-1 p-7">
       <h1 className="text-2xl font-semibold ">Home Page</h1>
-    </div>
+    </div> */}
 
     {/* rest code */}
 
-    <div className="">
+    <div className=" p-7">
             
             <div className='flex-auto '>
                 <div className=' flex flex-col bg-white'>
